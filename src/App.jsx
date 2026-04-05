aae5a4728cf0014e922214648c88b3a68035ff47
Dinesh7571 <cat505600@gmail.com>
2026-04-05 17:52:16 +0530

    jhj

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React, { useState } from 'react'
-import ProfileV1 from './components/Profile'
-import ProfileV2 from './components/Profile2'
+import Portfolio from './components/Profile'
 
 export default function App() {
   const [version, setVersion] = useState('v1') // default profile version
 
   return (
     <div className="min-h-screen bg-black text-white">
-      {version === 'v1' ? (
-        <ProfileV1 onSwitch={() => setVersion('v2')} />
-      ) : (
-        <ProfileV2 onSwitch={() => setVersion('v1')} />
-      )}
+      <Portfolio/>
     </div>
   )
 }
